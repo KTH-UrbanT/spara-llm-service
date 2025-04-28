dedcd5dc33cc1028c3b9ecc5a1f6c21dd66faf34
daniel <daniel.nordin14@gmail.com>
2025-04-28 22:15:19 +0200

    Mock data from csv

--- building_specs.py
+++ building_specs.py
@@ -1,14 +1,15 @@
 import re
-
+import pandas as pd
 class Spec:
-    def __init__(self, address, fnr, consumption, year):
+    def __init__(self, address, el_calc, varme_calc, basebent, stairs):
         self.address = address
-        self.fnr = fnr
-        self.energy_consumption = consumption
-        self.building_year = year
+        self.el_calc = el_calc
+        self.varm_calc = varme_calc
+        self.basement = basebent
+        self.stairs = stairs
 
     def __str__(self):
-        return f" Address: {self.address}, FNR: {self.fnr}, Energikonsumtion: {self.energy_consumption}, Byggnadsår: {self.building_year}"
+        return f" Address: {self.address}, Elkonsumtion: {self.el_calc}, Värmekonsumtion: {self.varm_calc}, Källarplan: {self.basement}, Antal Trapphus: {self.stairs}"
         
     building_id: str
     fnr: str
@@ -18,17 +19,23 @@ class Spec:
     building_year: int
     building_type: int
     atemp: float
-    number_of_floors: float
-    number_of_staircase: int
-    energy_consumption: str
+    stairs: float
+    stairs: int
+    el_calc: int
+    varm_calc: int
 
 
 def search_for_address(prompt, buildings):
-    address = ""
     pattern = re.compile(r"\b(?:bor\ på|address\ är)\b\W+(\w+(?:\W+\w+){0,1})", re.IGNORECASE)
     address = pattern.findall(prompt)
-    print(address[0])
-    return next((spec for spec in buildings if getattr(spec, "address", None).lower() == address[0].lower()), None)
+
+    if address:
+        print(address[0])
+        return next((spec for spec in buildings if getattr(spec, "address", None).lower() == address[0].lower()), None)
+    else:
+        print("No address found.")
+    return None
+
     
 
     
@@ -39,10 +46,21 @@ def search_for_address(prompt, buildings):
 
 
 def main(prompt):
-    spec1 = Spec("AddressGatan 5", "F1N2R3", "60KWh", 1998)
-    spec2 = Spec("blåkulla 3b", "F1N2R3", "80KWh", 1912)
-    spec3 = Spec("Vårvägen 77", "F1N2R3", "70KWh", 2003)
-    buildings = [spec1, spec2, spec3]
+    df = pd.read_csv("buildings.csv", sep=";", usecols=["IdAdr", "El_calc", "EgiVarme_calc", "EgenAntalKallarplan", "EgenAntalPlan", "EgenAntalTrapphus"], skipinitialspace=True)
+    buildings = []
+    for _, row in df.iterrows():
+        spec = Spec(
+            address=row["IdAdr"],
+            el_calc=row["El_calc"],
+            varme_calc=row["EgiVarme_calc"],
+            basebent=row["EgenAntalKallarplan"],
+            stairs=row["EgenAntalTrapphus"]
+        )
+        buildings.append(spec)
+    #spec1 = Spec("AddressGatan 5", "F1N2R3", "60KWh", 1998)
+    #spec2 = Spec("blåkulla 3b", "F1N2R3", "80KWh", 1912)
+    #spec3 = Spec("Vårvägen 77", "F1N2R3", "70KWh", 2003)
+    print(df)
     spec = search_for_address(prompt, buildings)
     #print(spec)
     return spec.__str__()
